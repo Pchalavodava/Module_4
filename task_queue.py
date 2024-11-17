16fcd1d5d5c4ba01f8a92b1f1511387aba5f15d7
Pchelovodova <pchelovodovaes@gmail.com>
2024-11-17 20:35:27 +0100

    Исправление метода get_add_task (replace_last_task)

--- task_queue.py
+++ task_queue.py
@@ -1,5 +1,7 @@
+import random
 from dataclasses import dataclass
-from typing import Any
+from pprint import pprint
+from typing import Optional
 
 
 @dataclass
@@ -13,12 +15,13 @@ class TaskQueue:
     """
 
     def __init__(self):
-        self.task_list: list[Any] = []
+        self.task_list: list[Task] = []
+        self.queue_len: int = 10
 
-    def add_task(self, task: Any) -> None:
+    def add_task(self, task: Task) -> None:
         """
         Добавление задачи в очередь
-        :param task: list[Any]: задача, добавляемая в очередь
+        :param task: list[Task]: задача, добавляемая в очередь
         :return: None
         """
         self.task_list.append(task)
@@ -30,24 +33,35 @@ class TaskQueue:
         """
         return not bool(self.task_list)
 
-    def get_next_task(self) -> Any:
+    def get_next_task(self) -> Optional[Task]:
         """
         Получение первой задачи из очереди
-        :return: Any: Задача, находящаяся первой в списке (очереди)
+        :return: Optional[Task]: Задача, находящаяся первой в списке (очереди), если очередь не пуста, иначе None
         """
         if not self.is_empty():
             return self.task_list.pop(0)
         else:
             return None
 
-    def get_add_task(self, task: Task) -> Any:
+    def get_last_task(self) -> Optional[Task]:
         """
-        Извлечение первой задачи при добавлении новой
-        :param task: Task: Новый элемент списка
-        :return: Any: Первый элемент в списке (очереди)
-        :raises ValueError: Если очередь пустая
+        Получение последней задачи из очереди
+        :return: Optional[Task]: Задача, находящаяся последней в списке (очереди), если очередь не пуста, иначе None
+        """
+        if not self.is_empty():
+            return self.task_list.pop()
+        else:
+            return None
 
-        >>> TaskQueue().get_add_task(Task('Моя задача 3'))
+    def replace_last_task(self, task: Task) -> Task:
+        """
+        Извлечение первой задачи для обработки из списка и добавление новой задачи в очередь на нужную позицию
+        :param task: Task: Новая задача
+        :return: Task: Первый элемент (задача) в списке (очереди)
+        :raises ValueError: Если очередь пустая
+        :raises ValueError: Если выбранная позиция превышает количество элементов
+
+        >>> TaskQueue().replace_last_task(Task('Моя задача 3'))
         Traceback (most recent call last):
         ...
         ValueError: Очередь пуста - извлекать нечего. Невозможно добавить задачу
@@ -55,15 +69,16 @@ class TaskQueue:
         if self.is_empty():
             raise ValueError('Очередь пуста - извлекать нечего. Невозможно добавить задачу')
         else:
+            replaced_task = self.get_last_task()
             self.add_task(task)
-            self.get_next_task()
+        return replaced_task
 
-    def if_queue_is_full(self) -> bool:
+    def is_full(self) -> bool:
         """
         Проверка, заполнена ли очередь. Предполагается, что максимальная длина = 10
         :return: bool: True, если заполнена, False - иначе
         """
-        return len(self.task_list) == 10
+        return len(self.task_list) == self.queue_len
 
 
 if __name__ == '__main__':
@@ -73,31 +88,30 @@ if __name__ == '__main__':
 
 queue = TaskQueue()
 
-task1 = Task("Задача 1")
-task2 = Task("Задача 2")
-task3 = Task("Задача 3")
-
-queue.add_task(task1)
-queue.add_task(task2)
-queue.add_task(task3)
+# task1 = Task("Задача 1")
+# task2 = Task("Задача 2")
+# task3 = Task("Задача 3")
+#
+# queue.add_task(task1)
+# queue.add_task(task2)
+# queue.add_task(task3)
 
 
 """
 Примеры для мною добавленных методов
 """
-# task_list = [Task("Задача 1"), Task("Задача 2"), Task("Задача 3"),
-#              Task("Задача 4"), Task("Задача 5"), Task("Задача 6"),
-#              Task("Задача 7"), Task("Задача 8"), Task("Задача 9"),
-#              Task("Задача 10"), Task("Задача 11"), Task("Задача 12")]
+task_list = [Task("Задача 1"), Task("Задача 2"), Task("Задача 3"),
+             Task("Задача 4"), Task("Задача 5"), Task("Задача 6"),
+             Task("Задача 7"), Task("Задача 8"), Task("Задача 9"),
+             Task("Задача 10"), Task("Задача 11"), Task("Задача 12")]
 
-# for task in task_list:
-#     if queue.if_queue_is_full():
-#         queue.get_add_task(task)
-#     else:
-#         queue.add_task(task)
-#
-# print(queue.task_list)
+for my_task in task_list:
+    if not queue.is_full():
+        queue.add_task(my_task)
 
+print(queue.replace_last_task(Task('Задача 15')))
+
+# pprint(queue.task_list)
 
 next_task = queue.get_next_task()
 print(f"Следующая задача: {next_task.name if next_task else 'Нет задач'}")
